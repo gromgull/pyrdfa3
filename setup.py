75334d90ce367d2432154fbaea92fed377fefec8
Gunnar Aastrand Grimnes <gromgull@gmail.com>
2012-09-25 13:04:45 +0200

    added explicit dependencies and rdflib entry_point registration if setuptools is available. Added rdflib parser

--- setup.py
+++ setup.py
@@ -1,4 +1,31 @@
-from distutils.core import setup
+import sys
+
+kwargs={}
+
+# fall back on no plugin-extensions and no dependency handling if setuptools 
+# is not installed.
+try:
+    from setuptools import setup
+    kwargs["install_requires"]=['rdflib', 'html5lib']
+
+    # simplejson needed if the JSON serialization is used and 
+    # if the underlying python version is 2.5 or lower
+    if sys.version_info.major==2 and sys.version_info.minor<6:
+        kwargs["install_requires"].append("simplejson")
+        
+    kwargs["entry_points"]={
+        'rdf.plugins.parser' : [ 'rdfa = pyRdfa.rdflib:RDFaParser',
+                                 'rdfa1.1 = pyRdfa.rdflib:RDFaParser',
+                                 'application/xhtml+xml = pyRdfa.rdflib:RDFaParser',
+                                 'application/svg+xml = pyRdfa.rdflib:RDFaParser',
+                                 'html = pyRdfa.rdflib:StructuredDataParser',
+                                 'text/html = pyRdfa.rdflib:StructuredDataParser',
+                                 ]
+        }
+
+except:
+    from distutils.core import setup
+
 setup(name="pyRdfa",
       description="pyRdfa Libray",
       version="3.4.3",
@@ -14,5 +41,6 @@ setup(name="pyRdfa",
 				'pyRdfaExtras',
 				'pyRdfaExtras.extras',
 				'pyRdfaExtras.serializers'
-				])
+				], 
+      **kwargs)
 
